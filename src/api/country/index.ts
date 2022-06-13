643205618298d03e2f2d3578a33ce451fc780954
Excession27 <feboobef27@gmail.com>
2022-06-13 13:46:56 +0200

    Changed String to string

--- src/api/country/index.ts
+++ src/api/country/index.ts
@@ -4,11 +4,11 @@ import { CountryType } from "./types";
 
 export const getAllCountries = (): APICall<CountryType[]> => axios.get(`/all`);
 
-export const getCountriesByName = (name: String): APICall<CountryType[]> =>
+export const getCountriesByName = (name: string): APICall<CountryType[]> =>
   axios.get(`/name/${name}`);
 
-export const getCountriesByCode = (code?: String): APICall<CountryType[]> =>
+export const getCountriesByCode = (code?: string): APICall<CountryType[]> =>
   axios.get(`/alpha/${code}`);
 
-export const getCountriesByRegion = (region: String): APICall<CountryType[]> =>
+export const getCountriesByRegion = (region: string): APICall<CountryType[]> =>
   axios.get(`/region/${region}`);
